27fcde3ddc627e4153a6dff964c2471d872efa98
김보운 <haioer@naver.com>
2021-03-01 19:19:24 +0900

    키 사이닝, 더블 클릭 방지 업데이트

--- app/src/main/java/com/bowoon/android/android_common_library/databinding/View.kt
+++ app/src/main/java/com/bowoon/android/android_common_library/databinding/View.kt
@@ -1,5 +1,6 @@
 package com.bowoon.android.android_common_library.databinding
 
+import android.os.SystemClock
 import android.view.View
 import androidx.databinding.BindingAdapter
 
@@ -24,3 +25,16 @@ fun View?.goneIf(condition: Boolean) {
         }
     }
 }
+
+@BindingAdapter("android:singleClick")
+fun View?.singleClick(action: () -> Unit) {
+    this?.let {
+        val currentTime = SystemClock.elapsedRealtime()
+
+        if (currentTime + TIME_INTERVAL < SystemClock.elapsedRealtime()) {
+            action.invoke()
+        }
+    }
+}
+
+private const val TIME_INTERVAL = 300
